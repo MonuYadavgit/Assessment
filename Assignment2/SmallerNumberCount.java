48d027a3e1b96950cbcdb5f8cb754a4c45ee1f88
MonuYadavgit <142574090+MonuYadavgit@users.noreply.github.com>
2023-08-18 17:09:06 +0530

    Create SmallerNumberCount.java

---
+++ Assignment2/SmallerNumberCount.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class SmallerNumbersCount {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+
+        int[] nums = new int[n];
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = scanner.nextInt();
+        }
+
+        int[] result = smallerNumbersThanCurrent(nums);
+
+        System.out.println("Counts of smaller numbers for each element:");
+        for (int count : result) {
+            System.out.print(count + " ");
+        }
+    }
+
+    public static int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] result = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            int count = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[j] < nums[i]) {
+                    count++;
+                }
+            }
+            result[i] = count;
+        }
+        return result;
+    }
+}
